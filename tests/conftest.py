0a5e51d6702f5483c166ab35873bc557158e43c0
Maximilian Mitchell <max@max.me.uk>
2019-05-01 21:02:58 +0100

    .

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,5 +2,5 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def no_requests(monkeypatch):
-    monkeypatch.delattr("camera.run")
+    monkeypatch.delattr("camera")
     monkeypatch.delattr("picamera")
